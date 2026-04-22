7d6f1d39f68a8e458b18f17915ce40b87723ad6f
Gabriel Custodio Boelter <gabriel.366610@alunosatc.edu.br>
2026-04-22 20:03:17 -0300

    Add Teste da Fake Store API

---
+++ test/fakestoreapi_test.ts
@@ -0,0 +1,93 @@
+const pactum = require('pactum');
+const { settings } = pactum;
+
+beforeAll(() => {
+  settings.setLogLevel('ERROR');
+});
+
+describe('Fake Store API - 10 Cenários de Testes', () => {
+  const BASE_URL = 'https://fakestoreapi.com';
+
+  it('1. Deve listar todos os produtos', async () => {
+    await pactum.spec()
+      .get(`${BASE_URL}/products`)
+      .expectStatus(200)
+      .expectJsonLike([]); 
+  });
+
+  it('2. Deve buscar um produto específico (ID 1)', async () => {
+    await pactum.spec()
+      .get(`${BASE_URL}/products/1`)
+      .expectStatus(200)
+      .expectJsonSchema({
+        "type": "object",
+        "properties": { "id": { "type": "number" } }
+      });
+  });
+
+  it('3. Deve limitar a quantidade de resultados (Limit 5)', async () => {
+    await pactum.spec()
+      .get(`${BASE_URL}/products`)
+      .withQueryParams('limit', 5)
+      .expectStatus(200)
+      .expectJsonLength(5); 
+  });
+
+  it('4. Deve ordenar os produtos por ordem decrescente (Sort Desc)', async () => {
+    await pactum.spec()
+      .get(`${BASE_URL}/products`)
+      .withQueryParams('sort', 'desc')
+      .expectStatus(200);
+  });
+
+it('5. Deve criar um novo produto (POST)', async () => {
+    await pactum.spec()
+      .post(`${BASE_URL}/products`)
+      .withJson({
+        title: 'Monitor Gamer',
+        price: 1500,
+        category: 'electronics'
+      })
+      .expectStatus(201); 
+});
+
+  it('6. Deve atualizar um produto via PUT', async () => {
+    await pactum.spec()
+      .put(`${BASE_URL}/products/1`)
+      .withJson({ title: 'Novo Título' })
+      .expectStatus(200);
+  });
+
+  it('7. Deve deletar um produto', async () => {
+    await pactum.spec()
+      .delete(`${BASE_URL}/products/1`)
+      .expectStatus(200);
+  });
+
+  it('8. Deve listar todas as categorias disponíveis', async () => {
+    await pactum.spec()
+      .get(`${BASE_URL}/products/categories`)
+      .expectStatus(200)
+      .expectJsonLike(["electronics", "jewelery"]);
+  });
+
+  it('9. Deve listar produtos de uma categoria específica (Jewelery)', async () => {
+    await pactum.spec()
+      .get(`${BASE_URL}/products/category/jewelery`)
+      .expectStatus(200);
+  });
+
+it('10. Deve realizar login de usuário com sucesso', async () => {
+    await pactum.spec()
+      .post(`${BASE_URL}/auth/login`)
+      .withJson({
+        username: 'mor_2314',
+        password: '83r5^_',
+      })
+      .expectStatus(201) 
+      .expectJsonLike({
+        token: /.+/ 
+      });
+});
+
+});
